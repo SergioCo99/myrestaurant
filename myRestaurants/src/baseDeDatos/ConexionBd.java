ffd34a8a79fcd37b1314213edeb1e25d7b58f2dc
jok <jok@localhost>
2019-11-19 16:57:00 +0100

    Falta hacer la bd propia

--- myRestaurants/src/baseDeDatos/ConexionBd.java
+++ myRestaurants/src/baseDeDatos/ConexionBd.java
@@ -1,5 +1,39 @@
 package baseDeDatos;
 
-public class ConexionBd {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-}//sa
+public class ConexionBd {
+	
+	private static final String CONTROLADOR = "com.mysql.cj.jdbc.Driver";
+	private static final String URL = "jdbc:mysql://localhost:3306/bd_ejemplo";
+	private static final String USUARIO = "root";
+	private static final String CLAVE = "Portu129";
+	
+	static {
+		try {
+			Class.forName(CONTROLADOR);//com.mysql.jdbc.Driver <- este es el que pone en la web, pero eclipse me recomienda utilizar este.	
+		} catch (ClassNotFoundException e) {
+			System.out.println("ERROR al cargar el controlador!");
+			e.printStackTrace();
+		}
+	}
+	
+	public Connection conectar() {
+		Connection conexion = null;
+		try {
+			conexion = DriverManager.getConnection(URL, USUARIO, CLAVE);
+			System.out.println("Conexion OK");
+		}catch(SQLException e) {
+			System.out.println("ERROR en la conexion!");
+			e.printStackTrace();
+		}
+		return conexion;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ myRestaurants/src/baseDeDatos/TestConexion.java
@@ -0,0 +1,16 @@
+package baseDeDatos;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class TestConexion {
+	public static void main(String[] args) {
+		ConexionBd conexion = new ConexionBd();
+		Connection cn = null;
+		Statement stm = null; 
+		
+		
+		
+	}
+	
+}
