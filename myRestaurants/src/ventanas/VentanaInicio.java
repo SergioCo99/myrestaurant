4bdad4bce6177739fa59691d13382afe74acd3a7
jokin <jokinme99@opendeusto.es>
2020-02-05 08:51:01 +0100

    Otra mejora: Properties de las transiciones entre ventanas

--- myRestaurants/src/ventanas/VentanaInicio.java
+++ myRestaurants/src/ventanas/VentanaInicio.java
@@ -2,10 +2,23 @@ package ventanas;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
+import alimentacion.Producto;
 import ventanas.ImagenInicio;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 import java.awt.*;
 
 public class VentanaInicio extends JFrame{
@@ -15,8 +28,44 @@ public class VentanaInicio extends JFrame{
 	JButton Jbt2;
 	JButton Jbt3;
 	JButton Hscr;
+	Properties properties = new Properties();
+	private static long MILISEGUNDOS_POR_VENTANA = 10;//Transición de ventanas
 	
 	public VentanaInicio(){
+		//Propiedades de la transición de ventanas
+		try {
+			properties.load(new FileInputStream("properties.xml"));
+			MILISEGUNDOS_POR_VENTANA = Long.parseLong(properties.getProperty("MILISEGUNDOS_POR_VENTANA"));
+		}catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		VentanaInicio ventana = new VentanaInicio();
+		
+		//Propiedades de la ventana
+		ventana.addWindowListener(new WindowAdapter() {
+			
+			@Override
+			public void windowClosed(WindowEvent e1) {
+				properties.setProperty("MILISEGUNDOS_POR_VENTANA", "" + MILISEGUNDOS_POR_VENTANA);
+				try {
+					properties.store(new FileOutputStream("properties.xml"), "propiedades");
+				}catch(IOException e) {
+					e.printStackTrace();
+				}
+				try {
+					properties.load(new FileInputStream("properties.xml"));
+					properties.get("transición entre ventanas");
+				}catch(FileNotFoundException e) {
+					e.printStackTrace();
+				}catch(IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		ImagenInicio v = new ImagenInicio();
